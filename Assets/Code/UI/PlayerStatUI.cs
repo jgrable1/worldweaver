79fe5fd836850ee0fcd62b9d2e9854a181b2fd52
BijanT123 <157776292+BijanT123@users.noreply.github.com>
2024-11-24 12:13:56 -0500

    UI Update/GameWorld changes
    Updated UI for clarity and saved changes to game world

--- Assets/Code/UI/PlayerStatUI.cs
+++ Assets/Code/UI/PlayerStatUI.cs
@@ -9,16 +9,16 @@ public class PlayerStatUI : MonoBehaviour
     public void updateStat(int currStat){
         float newBarWidth = currStat * barWidth / playerMaxStat;
         RectTransformComponent.sizeDelta = new Vector2(newBarWidth, barHeight);
-        statAmount.text = currStat + "/" + playerMaxStat;
+        statAmount.text = "HP: " + currStat + "/" + playerMaxStat;
     }
     public void updateStat(float currStat){
         float newBarWidth = currStat * barWidth / playerMaxStat;
         RectTransformComponent.sizeDelta = new Vector2(newBarWidth, barHeight);
-        statAmount.text = Mathf.Round(10*currStat) + "%";
+        statAmount.text = "Run: " + Mathf.Round(10*currStat) + "%";
     }
 
     public void updateDash(bool active){
-        statAmount.text = active ? "Active" : "Inactive";
+        statAmount.text = "Dash: " + (active ? "Active" : "Inactive");
         RectTransformComponent.sizeDelta = active ? new Vector2(barWidth, barHeight) : new Vector2(0,0);
     }
 }

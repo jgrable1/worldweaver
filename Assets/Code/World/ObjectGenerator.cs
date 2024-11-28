0181c3972553166cba875b5082a2d2c781eecedc
ncurtis3 <143731446+ncurtis3@users.noreply.github.com>
2024-11-27 20:58:41 -0500

    Fixing a bug with trees being deleted after running the game once

--- Assets/Code/World/ObjectGenerator.cs
+++ Assets/Code/World/ObjectGenerator.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class ObjectGenerator : MonoBehaviour
 {
     public GameObject Tree;
-    // Start is called before the first frame update
+    TerrainData thisTerrain;
+    TreeInstance[] originalTrees;
     void Start()
     {
         //The following code was inspired by user "hockenmaier" on the Unity forums. We have made modifications to the code from them but the main purpose of the code is the same.
         //A link to the discussion where this was posted is as follows: https://discussions.unity.com/t/solved-placing-trees-with-scripts-on-terrain/659480/16
 
         // Grab the island's terrain data
-        TerrainData thisTerrain;
         thisTerrain = GetComponent<Terrain>().terrainData;
 
         //GameObject to be the parent
         GameObject treeParent = new GameObject("treeParent");
+
+        originalTrees = thisTerrain.treeInstances;
 
         // For every terrain tree on the island
         foreach (TreeInstance terrainTree in thisTerrain.treeInstances)
@@ -28,11 +30,13 @@ public class ObjectGenerator : MonoBehaviour
             GameObject prefabTree = Instantiate(Tree, worldTreePos, Quaternion.identity, treeParent.transform); // Create a prefab tree on its pos
 
             //Then set the new tree to the randomized size and rotation of the terrain tree
-            prefabTree.transform.localScale = new Vector3(terrainTree.widthScale * 50, terrainTree.heightScale * 50, terrainTree.widthScale * 50);
-            prefabTree.transform.rotation = Quaternion.AngleAxis(terrainTree.rotation * 57.2958f, Vector3.up);
+            prefabTree.transform.localScale = new Vector3(terrainTree.widthScale, terrainTree.heightScale, terrainTree.widthScale);
+            prefabTree.transform.rotation = Quaternion.AngleAxis(terrainTree.rotation, Vector3.up);
         }
-        // Then delete all the terrain trees on the island
-        List<TreeInstance> newTrees = new List<TreeInstance>(0);
-        thisTerrain.treeInstances = newTrees.ToArray();
+        thisTerrain.treeInstances = new TreeInstance[0];
+    }
+
+    void OnApplicationQuit() {
+        thisTerrain.treeInstances = originalTrees;
     }
 }

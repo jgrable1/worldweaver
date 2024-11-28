190b5bc5bbb77dd9d47de001141652ce67821a7c
ncurtis3 <143731446+ncurtis3@users.noreply.github.com>
2024-11-27 20:44:05 -0500

    Unbreaking things that I broke

--- Assets/Code/World/ObjectGenerator.cs
+++ Assets/Code/World/ObjectGenerator.cs
@@ -4,33 +4,35 @@ using UnityEngine;
 
 public class ObjectGenerator : MonoBehaviour
 {
-    public GameObject ObjectToCreate;
-    public int seed;
-    public int numToPlace;
+    public GameObject Tree;
     // Start is called before the first frame update
     void Start()
     {
-        //The following code was inspired by user "hockenmaier" on the Unity forums. We have made modifications to it that have changed the fundamentals of what hockenmaier was doing.
+        //The following code was inspired by user "hockenmaier" on the Unity forums. We have made modifications to the code from them but the main purpose of the code is the same.
         //A link to the discussion where this was posted is as follows: https://discussions.unity.com/t/solved-placing-trees-with-scripts-on-terrain/659480/16
 
-        System.Random random = new System.Random(seed);
-
-        TerrainData thisTerrain = GetComponent<Terrain>().terrainData;
+        // Grab the island's terrain data
+        TerrainData thisTerrain;
+        thisTerrain = GetComponent<Terrain>().terrainData;
 
         //GameObject to be the parent
         GameObject treeParent = new GameObject("treeParent");
-        RaycastHit hit;
 
         // For every terrain tree on the island
-        for (int i = 0; i < numToPlace; i++) {
-            Vector3 randomPoint = new Vector3(random.Next(0, 1000), 250, random.Next(0, 1000));
-            Physics.Raycast(randomPoint, -Vector3.up, out hit);
+        foreach (TreeInstance terrainTree in thisTerrain.treeInstances)
+        {
+            // Find its local position scaled by the terrain size (to find the real world position)
+            Vector3 worldTreePos = Vector3.Scale(terrainTree.position, thisTerrain.size) + Terrain.activeTerrain.transform.position;
 
             // Create the new tree out of the prefab
-            GameObject prefabTree = Instantiate(ObjectToCreate, hit.point, Quaternion.identity, treeParent.transform); // Create a prefab tree on its pos
+            GameObject prefabTree = Instantiate(Tree, worldTreePos, Quaternion.identity, treeParent.transform); // Create a prefab tree on its pos
 
             //Then set the new tree to the randomized size and rotation of the terrain tree
-            prefabTree.transform.rotation = Quaternion.AngleAxis(random.Next(0, 360), Vector3.up);
+            prefabTree.transform.localScale = new Vector3(terrainTree.widthScale * 50, terrainTree.heightScale * 50, terrainTree.widthScale * 50);
+            prefabTree.transform.rotation = Quaternion.AngleAxis(terrainTree.rotation * 57.2958f, Vector3.up);
         }
+        // Then delete all the terrain trees on the island
+        List<TreeInstance> newTrees = new List<TreeInstance>(0);
+        thisTerrain.treeInstances = newTrees.ToArray();
     }
 }

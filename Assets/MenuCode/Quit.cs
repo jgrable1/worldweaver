3355717510074d0b886919ea414f507f7040221d
BijanT123 <157776292+BijanT123@users.noreply.github.com>
2024-11-30 00:23:56 -0500

    Improved End Screens
    Replaced filler text and fixed background bug.

--- Assets/MenuCode/Quit.cs
+++ Assets/MenuCode/Quit.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class Quit : MonoBehaviour
 {
-    ObjectGenerator needRestore;
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space)){
             UnityEditor.EditorApplication.isPlaying = false;
             Application.Quit();
-            needRestore.RestoreTrees();
         }
     }
 }

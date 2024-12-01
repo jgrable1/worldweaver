6e14c3c5e8162ec4530fb2d989a6adb96a8a640f
ncurtis3 <143731446+ncurtis3@users.noreply.github.com>
2024-11-30 20:50:50 -0500

    In the middle of changes, just backing up to git so that I don't lose progress if something dies

--- Assets/Code/World/ObjectGenerator.cs
+++ Assets/Code/World/ObjectGenerator.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class ObjectGenerator : MonoBehaviour
 {
     public GameObject Tree;
-    public BasicItem Wood;
     public World World;
     TerrainData thisTerrain;
     TreeInstance[] originalTrees;
@@ -34,7 +33,6 @@ public class ObjectGenerator : MonoBehaviour
             //Then set the new tree to the randomized size and rotation of the terrain tree
             prefabTree.transform.localScale = new Vector3(terrainTree.widthScale, terrainTree.heightScale, terrainTree.widthScale);
             prefabTree.transform.rotation = Quaternion.AngleAxis(terrainTree.rotation, Vector3.up);
-            prefabTree.GetComponent<ResourceNode>().SetResource(Wood);
             prefabTree.GetComponent<ResourceNode>().SetWorld(World);
         }
 
